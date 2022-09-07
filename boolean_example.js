ab1cf6731e546bf89f015cf7d3553f99bb02ac86
SolarFlurry <92278004+SolarFlurry@users.noreply.github.com>
2022-09-07 20:37:49 +1000

    Add files via upload

---
+++ boolean_example.js
@@ -0,0 +1,30 @@
+class BooleanExample {
+    constructor() {
+    }
+    
+    getInfo() {
+        return {
+            "id": "Booleans",
+            "name": "Standard Booleans",
+            "blocks": [ {
+                            "opcode": "rtrue",
+                            "blockType": "reporter",
+                            "text": "true",
+                            }
+                        },{"opcode": "rfalse",
+                            "blockType": "reporter",
+                            "text": "false",
+                            }
+                        }, ]
+        }
+    }
+
+    rtrue(){
+		return true
+	}
+	rfalse(){
+		return false
+	}
+}
+
+Scratch.extensions.register(new BooleanExample())
